e206c96aa94d83dbf9a4e1499f28f097bf79b72a
PipeRojas <andres.rojas-o@mail.escuelaing.edu.co>
2017-05-17 12:38:38 -0500

    Start

--- src/main/java/com/example/demoApplication.java
+++ src/main/java/com/example/demoApplication.java
@@ -45,7 +45,7 @@ public class demoApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
 			}
 		};
 	}
